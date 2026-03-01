a1c7c91bda08d86614847113a5cac246cda62aae
无心 <ajun8849@gmail.com>
2026-03-01 05:18:31 +0000

    关于 SDK 设置和配置 以我们的web项目应该选哪个

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,16 @@
+/**
+ * Firebase 客户端配置
+ * 
+ * 在 Firebase 控制台选择 "npm" 选项后，请确保以下配置与控制台显示的配置一致。
+ * 项目已配置为使用 Next.js 15 + App Hosting。
+ */
+
 export const firebaseConfig = {
-  "projectId": "studio-7030069084-9fdd8",
-  "appId": "1:289818699636:web:c910bb42d795e632e8ee21",
-  "apiKey": "AIzaSyCZUoStO6bw63-WYszz2yqCIObgQvxg4tI",
-  "authDomain": "studio-7030069084-9fdd8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "289818699636"
+  apiKey: "AIzaSyCZUoStO6bw63-WYszz2yqCIObgQvxg4tI",
+  authDomain: "studio-7030069084-9fdd8.firebaseapp.com",
+  projectId: "studio-7030069084-9fdd8",
+  storageBucket: "studio-7030069084-9fdd8.firebasestorage.app",
+  messagingSenderId: "289818699636",
+  appId: "1:289818699636:web:c910bb42d795e632e8ee21",
+  measurementId: ""
 };
